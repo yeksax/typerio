58264e847daf8fcf33bed6ce4b508651b94b6ab5
yeskax <hugoalmeida2412@gmail.com>
2023-07-08 22:42:49 -0300

    style: defined height to follow button

--- src/components/UserComponents/FollowButton.tsx
+++ src/components/UserComponents/FollowButton.tsx
@@ -48,7 +48,7 @@ export function FollowButton({
 				}}
 				onMouseEnter={() => setIsHovering(true)}
 				onMouseLeave={() => setIsHovering(false)}
-				className='text-xs cursor-pointer px-3 bg-white dark:bg-zinc-950 grid place-items-center py-0.5 text-black dark:text-zinc-50 rounded-md hover:text-white hover:bg-black transition-all border-black dark:hover:bg-zinc-800 dark:border-zinc-950 border-2'
+				className='text-xs h-6 cursor-pointer px-3 bg-white dark:bg-zinc-950 grid place-items-center py-0.5 text-black dark:text-zinc-50 rounded-md hover:text-white hover:bg-black transition-all border-black dark:hover:bg-zinc-800 dark:border-zinc-950 border-2'
 			>
 				{isHovering ? "Unfollow" : "Seguindo"}
 			</button>
@@ -65,7 +65,7 @@ export function FollowButton({
 				);
 				await followUser(target, user);
 			}}
-			className='text-xs cursor-pointer px-3 bg-white dark:bg-zinc-800 grid place-items-center py-0.5 text-black dark:text-zinc-50 rounded-md hover:text-white hover:bg-black transition-all border-black dark:hover:bg-zinc-950 dark:border-zinc-950 border-2'
+			className='text-xs h-6 cursor-pointer px-3 bg-white dark:bg-zinc-800 grid place-items-center py-0.5 text-black dark:text-zinc-50 rounded-md hover:text-white hover:bg-black transition-all border-black dark:hover:bg-zinc-950 dark:border-zinc-950 border-2'
 		>
 			Seguir
 		</button>
