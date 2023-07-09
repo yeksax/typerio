e7252a37c02069e4e1e28219fa01c9b9e0d5793b
yeskax <hugoalmeida2412@gmail.com>
2023-07-08 22:36:59 -0300

    fix: properly treated username during user update

--- src/app/(typer)/settings/profile/actions.tsx
+++ src/app/(typer)/settings/profile/actions.tsx
@@ -3,6 +3,11 @@
 import { authOptions } from "@/services/auth";
 import { prisma } from "@/services/prisma";
 import { _User } from "@/types/interfaces";
+import {
+	removeAccents,
+	removeBadCharacteres,
+	removeEmojis,
+} from "@/utils/general/string";
 import { getServerSession } from "next-auth";
 
 export async function updateProfile({
@@ -18,9 +23,31 @@ export async function updateProfile({
 
 	const links = e.getAll("link") as string[];
 	const displayName = e.get("displayName") as string;
-	const name = e.get("name") as string;
-	const tag = e.get("tag") as string;
 	const description = e.get("description") as string;
+	let name = e.get("name") as string;
+	let tag = e.get("tag") as string;
+
+	name = removeAccents(
+		removeBadCharacteres(
+			removeEmojis(
+				name
+					.toLowerCase()
+					.trim()
+					.replace(/\s/g, "-")
+			)
+		)
+	)
+
+	tag = removeAccents(
+		removeBadCharacteres(
+			removeEmojis(
+				tag
+					.toLowerCase()
+					.trim()
+					.replace(/\s/g, "0")
+			)
+		)
+	)
 
 	if (!session) return;
 	if (!session.user) return;
@@ -30,13 +57,11 @@ export async function updateProfile({
 			id: session.user.id,
 		},
 		data: {
-			username: `${name.toLowerCase().trim()}#${tag
-				.toLowerCase()
-				.trim()}`,
+			username: `${name}_${tag}`,
 			name: name.trim(),
 			links: links.map((link) => link.trim()),
 			displayName: displayName != "" ? displayName.trim() : null,
-			tag: tag.trim().padStart(4, "0"),
+			tag: tag,
 			biography: description.trim(),
 			banner: bannerURL,
 			avatar: avatarURL,
